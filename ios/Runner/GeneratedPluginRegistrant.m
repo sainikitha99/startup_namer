2ab5af3b27848369af6f79c41b961ce842a6c7ca
sainikitha99 <sainikitha.dmm@gmail.com>
2019-03-28 10:29:41 +0530

    commit 1

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <firebase_analytics/FirebaseAnalyticsPlugin.h>
+#import <firebase_core/FirebaseCorePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseAnalyticsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAnalyticsPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+}
+
+@end
